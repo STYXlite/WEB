d528036090e9fe02178503bd40e3284eda2fe8db
Next <145189893+HamzehJarrar@users.noreply.github.com>
2024-06-14 19:13:07 +0300

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="login">
+          <div class="tittle">
+            <h1>Student Login</h1>
+          </div>
+
+          <div class="pro">
+            <img src="assets/img/Login/3712238-middle 1.png" alt="" />
+
+            <div class="input-field">
+              <div class="email">
+                <input
+                  type="email"
+                  placeholder="User"
+                  class="input-email"
+                  id="Username"
+                />
+                <img src="assets/img/Login/person.svg" alt="person" />
+              </div>
+
+              <div class="email">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  class="input-Password"
+                  id="Password"
+                />
+                <img src="assets/img/Login/lock.svg" alt="person" />
+              </div>
+
+              <div class="submit">
+                <input type="submit" value="Login" id="BTN"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
